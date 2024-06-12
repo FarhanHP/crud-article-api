b462517d07868c704081ecc4b179d01874b63633
farhanhp <farhan.pradana@gdn-commerce.com>
2024-06-12 22:21:45 +0700

    init project

---
+++ src/main/java/com/farhanhp/crud_article_api/CrudArticleApiApplication.java
@@ -0,0 +1,13 @@
+package com.farhanhp.crud_article_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudArticleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudArticleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farhanhp/crud_article_api/CrudArticleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhanhp.crud_article_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudArticleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
