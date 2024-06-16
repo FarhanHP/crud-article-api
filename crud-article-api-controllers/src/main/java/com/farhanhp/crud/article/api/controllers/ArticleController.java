996959a0c28eb99d35ab37524c234a0126fa89cc
farhanhp <farhan.herdia123@gmail.com>
2024-06-16 23:23:09 +0700

    feat: enable cors

--- crud-article-api-controllers/src/main/java/com/farhanhp/crud/article/api/controllers/ArticleController.java
+++ crud-article-api-controllers/src/main/java/com/farhanhp/crud/article/api/controllers/ArticleController.java
@@ -20,6 +20,7 @@ import com.farhanhp.crud.article.api.models.web.response.WebResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,6 +38,7 @@ import com.farhanhp.crud.article.api.models.web.response.WebResponse;
 @RestController
 @RequestMapping(value = "/api/article", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
+@CrossOrigin
 public class ArticleController {
 
   private final CommandExecutor executor;
